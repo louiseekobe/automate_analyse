c5913edfc78352550704cb19873e4f1b054180ac
louiseekobe <77247913+louiseekobe@users.noreply.github.com>
2025-05-08 12:27:04 +0200

    Add files via upload

--- contain_pages/analyse_by_fmp.py
+++ contain_pages/analyse_by_fmp.py
@@ -18,15 +18,14 @@ def show():
 
     # 🔽 Sélection du FMP à analyser
     colnames = df_fmr.columns.tolist()
-    selected_fmp_col = st.selectbox("🏷️ Colonne contenant les FMP :", colnames, key="selected_fmp_col")
-
-    if selected_fmp_col:
-        fmp_list = df_fmr[selected_fmp_col].dropna().unique().tolist()
-        selected_fmp_value = st.selectbox("📍 Sélectionnez un FMP :", sorted(fmp_list), key="selected_fmp_value")
-
-        # 🔁 DataFrames filtrés pour ce FMP uniquement
-        df_fmr_filtered = df_fmr[df_fmr[selected_fmp_col] == selected_fmp_value]
-        df_merged_filtered = df_merged[df_merged[selected_fmp_col] == selected_fmp_value]
+    selected_fmp_col_01 = st.selectbox("🏷️ Colonne contenant les FMP :", colnames, key="selected_fmp_col_01")
+    if selected_fmp_col_01:
+        fmp_list = df_fmr[selected_fmp_col_01].dropna().unique().tolist()
+        selected_fmp_value_01 = st.selectbox("📍 Sélectionnez un FMP :", sorted(fmp_list), key="selected_fmp_value_01")
+            # 🔁 DataFrames filtrés pour ce FMP uniquement
+        if st.button("Selctionner le point de suivi des flux"): 
+            df_fmr_filtered = df_fmr[df_fmr[selected_fmp_col_01] == selected_fmp_value_01]
+            df_merged_filtered = df_merged[df_merged[selected_fmp_col_01] == selected_fmp_value_01]
     else:
         st.warning("⚠️ Aucune colonne FMP détectée.")
         return
@@ -52,10 +51,10 @@ def show():
                     del st.session_state[state_key]
 
     if st.session_state.active_analysis == "personnes par fmp":
-        st.markdown(f"### 👥 Analyse du nombre de personnes déplacées : {selected_fmp_value}")
+        st.markdown(f"### 👥 Analyse du nombre de personnes déplacées : {selected_fmp_value_01}")
         colnames = df_fmr_filtered.columns.tolist()
-        count_col = st.selectbox("🔢 Colonne : nombre total de personnes", colnames, key="selected_count")
-        date_col = st.selectbox("📅 Colonne : date de l'enquête", colnames, key="selected_date")
+        count_col = st.selectbox("🔢 Colonne : nombre total de personnes", colnames, key="selected_count_01")
+        date_col = st.selectbox("📅 Colonne : date de l'enquête", colnames, key="selected_date_01")
         if st.button("Lancer l'analyse"):   
             if count_col and date_col:
                 try:
@@ -71,31 +70,31 @@ def show():
                     st.error(f"Erreur : {e}")
 
     elif st.session_state.active_analysis == "fmp selectionne":
-        st.markdown(f"### 📦 Informations FMP : {selected_fmp_value}")
+        st.markdown(f"### 📦 Informations FMP : {selected_fmp_value_01}")
         colnames = df_fmr_filtered.columns.tolist()
-        fmp_col = st.selectbox("🏷️ Colonne : nom FMP", colnames, key="selected_fmp")
-        count_col = st.selectbox("🔢 Colonne : nombre total de personnes", colnames, key="selected_count_fmp")
-        type_flux_col = st.selectbox("🔄 Colonne : type de flux", colnames, key="selected_flux_fmp")
+        fmp_col = st.selectbox("🏷️ Colonne : nom FMP", colnames, key="selected_fmp_0101")
+        count_col = st.selectbox("🔢 Colonne : nombre total de personnes", colnames, key="selected_count_fmp_0101")
+        type_flux_col = st.selectbox("🔄 Colonne : type de flux", colnames, key="selected_flux_fmp_0101")
         if st.button("Lancer l'analyse"):    
             if fmp_col and count_col and type_flux_col:
                 try:
                     df_fmr_filtered[count_col] = pd.to_numeric(df_fmr_filtered[count_col], errors='coerce')
                     grouped = df_fmr_filtered.dropna(subset=[type_flux_col, count_col]) \
                                      .groupby(type_flux_col)[count_col].sum()
-                    st.markdown(f"#### 🔄 Nombre de personnes par type de flux pour : {selected_fmp_value}")
+                    st.markdown(f"#### 🔄 Nombre de personnes par type de flux pour : {selected_fmp_value_01}")
                     st.dataframe(grouped)
                     pourcentages = round((grouped / grouped.sum()) * 100, 2)
-                    st.markdown(f"#### 📊 Pourcentage par type de flux pour : {selected_fmp_value}")
+                    st.markdown(f"#### 📊 Pourcentage par type de flux pour : {selected_fmp_value_01}")
                     st.dataframe(pourcentages)
                 except Exception as e:
                     st.error(f"Erreur : {e}")
 
     elif st.session_state.active_analysis == "nationalites par fmp":
-        st.markdown(f"### 🌐 Nationalités pour FMP : {selected_fmp_value}")
+        st.markdown(f"### 🌐 Nationalités pour FMP : {selected_fmp_value_01}")
         colnames = df_merged_filtered.columns.tolist()
-        nat_col = st.selectbox("🌍 Colonne : nationalité", colnames, key="selected_nat")
-        count_col = st.selectbox("🔢 Colonne : nombre de personnes", colnames, key="selected_nat_count")
-        flux_col = st.selectbox("🔄 Colonne : type de flux", colnames, key="selected_nat_flux")
+        nat_col = st.selectbox("🌍 Colonne : nationalité", colnames, key="selected_nat_01")
+        count_col = st.selectbox("🔢 Colonne : nombre de personnes", colnames, key="selected_nat_count_01")
+        flux_col = st.selectbox("🔄 Colonne : type de flux", colnames, key="selected_nat_flux_01")
         if st.button("Lancer l'analyse"):    
             if nat_col and count_col and flux_col:
                 try:
@@ -113,91 +112,91 @@ def show():
                     )
                     total_all = pivot.values.sum()
                     pivot_percent = round((pivot / total_all) * 100, 2)
-                    st.markdown(f"#### 📉 Répartition (%) par flux et nationalité dans : {selected_fmp_value}")
+                    st.markdown(f"#### 📉 Répartition (%) par flux et nationalité dans : {selected_fmp_value_01}")
                     st.dataframe(pivot_percent.sort_values(by=pivot_percent.columns[0], ascending=False))
                 except Exception as e:
                     st.error(f"Erreur : {e}")
 
     elif st.session_state.active_analysis == "profil par fmp":
-        st.markdown(f"### 🧭 Profil des voyageurs pour FMP : {selected_fmp_value}")
+        st.markdown(f"### 🧭 Profil des voyageurs pour FMP : {selected_fmp_value_01}")
         colnames = df_fmr_filtered.columns.tolist()
-        selected_homme_col = st.selectbox("👨 Colonne : hommes 18-59 ans", colnames, key="homme_col")
-        selected_femme_col = st.selectbox("👩 Colonne : femmes 18-59 ans", colnames, key="femme_col")
-        selected_fille_col = st.selectbox("👧 Colonne : filles", colnames, key="fille_col")
-        selected_garcon_col = st.selectbox("👦 Colonne : garçons", colnames, key="garcon_col")
-        selected_homme60_col = st.selectbox("👴 Colonne : hommes 60+", colnames, key="homme60_col")
-        selected_femme60_col = st.selectbox("👵 Colonne : femmes 60+", colnames, key="femme60_col")
+        selected_homme_col_01  = st.selectbox("👨 Colonne : hommes 18-59 ans", colnames, key="homme_col_01")
+        selected_femme_col_01  = st.selectbox("👩 Colonne : femmes 18-59 ans", colnames, key="femme_col_01")
+        selected_fille_col_01  = st.selectbox("👧 Colonne : filles", colnames, key="fille_col_01")
+        selected_garcon_col_01 = st.selectbox("👦 Colonne : garçons", colnames, key="garcon_col_01")
+        selected_homme60_col_01 = st.selectbox("👴 Colonne : hommes 60+", colnames, key="homme60_col_01")
+        selected_femme60_col_01 = st.selectbox("👵 Colonne : femmes 60+", colnames, key="femme60_col_01")
         # Vérifier que toutes les colonnes sont sélectionnées
         if not all([
-            selected_homme_col, selected_femme_col, selected_fille_col,
-            selected_garcon_col, selected_homme60_col, selected_femme60_col
+            selected_homme_col_01, selected_femme_col_01, selected_fille_col_01,
+            selected_garcon_col_01, selected_homme60_col_01, selected_femme60_col_01
             ]):
             st.warning("⚠️ Veuillez sélectionner toutes les colonnes nécessaires.")
         else:
             if st.button("Lancer l'analyse"):    
                 try:
                     # Conversion en numérique
-                    df_fmr_filtered[selected_homme_col]   = pd.to_numeric(df_fmr_filtered[selected_homme_col], errors='coerce')
-                    df_fmr_filtered[selected_femme_col]   = pd.to_numeric(df_fmr_filtered[selected_femme_col], errors='coerce')
-                    df_fmr_filtered[selected_fille_col]   = pd.to_numeric(df_fmr_filtered[selected_fille_col], errors='coerce')
-                    df_fmr_filtered[selected_garcon_col]  = pd.to_numeric(df_fmr_filtered[selected_garcon_col], errors='coerce')
-                    df_fmr_filtered[selected_homme60_col] = pd.to_numeric(df_fmr_filtered[selected_homme60_col], errors='coerce')
-                    df_fmr_filtered[selected_femme60_col] = pd.to_numeric(df_fmr_filtered[selected_femme60_col], errors='coerce')
+                    df_fmr_filtered[selected_homme_col_01]   = pd.to_numeric(df_fmr_filtered[selected_homme_col_01], errors='coerce')
+                    df_fmr_filtered[selected_femme_col_01]   = pd.to_numeric(df_fmr_filtered[selected_femme_col_01], errors='coerce')
+                    df_fmr_filtered[selected_fille_col_01]   = pd.to_numeric(df_fmr_filtered[selected_fille_col_01], errors='coerce')
+                    df_fmr_filtered[selected_garcon_col_01]  = pd.to_numeric(df_fmr_filtered[selected_garcon_col_01], errors='coerce')
+                    df_fmr_filtered[selected_homme60_col_01] = pd.to_numeric(df_fmr_filtered[selected_homme60_col_01], errors='coerce')
+                    df_fmr_filtered[selected_femme60_col_01] = pd.to_numeric(df_fmr_filtered[selected_femme60_col_01], errors='coerce')
                     # Total global
                     total = st.session_state.get("total_personnes")
                     if total is None or total == 0:
                         total = (
-                            df_fmr_filtered[selected_homme_col].sum() +
-                            df_fmr_filtered[selected_femme_col].sum() +
-                            df_fmr_filtered[selected_fille_col].sum() +
-                            df_fmr_filtered[selected_garcon_col].sum() +
-                            df_fmr_filtered[selected_homme60_col].sum() +
-                            df_fmr_filtered[selected_femme60_col].sum()
+                            df_fmr_filtered[selected_homme_col_01].sum() +
+                            df_fmr_filtered[selected_femme_col_01].sum() +
+                            df_fmr_filtered[selected_fille_col_01].sum() +
+                            df_fmr_filtered[selected_garcon_col_01].sum() +
+                            df_fmr_filtered[selected_homme60_col_01].sum() +
+                            df_fmr_filtered[selected_femme60_col_01].sum()
                         )
                     # Calculs
-                    pvo = df_fmr_filtered[selected_femme60_col].sum() + df_fmr_filtered[selected_homme60_col].sum()
-                    homme  = round((df_fmr_filtered[selected_homme_col].sum() + df_fmr_filtered[selected_homme60_col].sum()) / total * 100, 2)
-                    femme  = round((df_fmr_filtered[selected_femme_col].sum() + df_fmr_filtered[selected_femme60_col].sum()) / total * 100, 2)
-                    garcon = round(df_fmr_filtered[selected_garcon_col].sum() / total * 100, 2)
-                    fille  = round(df_fmr_filtered[selected_fille_col].sum() / total * 100, 2)
+                    pvo = df_fmr_filtered[selected_femme60_col_01].sum() + df_fmr_filtered[selected_homme60_col_01].sum()
+                    homme  = round((df_fmr_filtered[selected_homme_col_01].sum() + df_fmr_filtered[selected_homme60_col_01].sum()) / total * 100, 2)
+                    femme  = round((df_fmr_filtered[selected_femme_col_01].sum() + df_fmr_filtered[selected_femme60_col_01].sum()) / total * 100, 2)
+                    garcon = round(df_fmr_filtered[selected_garcon_col_01].sum() / total * 100, 2)
+                    fille  = round(df_fmr_filtered[selected_fille_col_01].sum() / total * 100, 2)
                     # Affichage
-                    st.success(f"👥 Personnes vulnérables (60+) de  {selected_fmp_value} : **{int(pvo)}**")
-                    st.info(f"👨 Hommes (avec 60+) de : {selected_fmp_value} : **{homme}%**")
-                    st.info(f"👩 Femmes (avec 60+) de : {selected_fmp_value} : **{femme}%**")
-                    st.info(f"👦 Garçons de : {selected_fmp_value} : **{garcon}%**")
-                    st.info(f"👧 Filles  de : {selected_fmp_value}: **{fille}%**")
+                    st.success(f"👥 Personnes vulnérables (60+) de  {selected_fmp_value_01} : **{int(pvo)}**")
+                    st.info(f"👨 Hommes (avec 60+) de : {selected_fmp_value_01} : **{homme}%**")
+                    st.info(f"👩 Femmes (avec 60+) de : {selected_fmp_value_01} : **{femme}%**")
+                    st.info(f"👦 Garçons de : {selected_fmp_value_01} : **{garcon}%**")
+                    st.info(f"👧 Filles  de : {selected_fmp_value_01}: **{fille}%**")
                 except Exception as e:
                     st.error(f"❌ Erreur lors du traitement : {e}")
 
     elif st.session_state.active_analysis == "depart par fmp":
-        st.markdown(f"### 🏁 Pays de départ pour FMP : {selected_fmp_value}")
+        st.markdown(f"### 🏁 Pays de départ pour FMP : {selected_fmp_value_01}")
         st.markdown("### 🏁 Pays de départ")
         colnames = df_fmr_filtered.columns.tolist()
-        depart_col = st.selectbox("🌍 Colonne : pays de départ", colnames, key="depart_col")
-        depart_count_col = st.selectbox("🔢 Colonne : nombre de personnes", colnames, key="depart_count_col")
+        depart_col = st.selectbox("🌍 Colonne : pays de départ", colnames, key="depart_col_01")
+        depart_count_col = st.selectbox("🔢 Colonne : nombre de personnes", colnames, key="depart_count_col_01")
         if st.button("Lancer l'analyse"):    
             if depart_col and depart_count_col:
                 try:
                     df_fmr_filtered[depart_count_col] = pd.to_numeric(df_fmr_filtered[depart_count_col], errors='coerce')
                     grouped = df_fmr_filtered.groupby(depart_col)[depart_count_col].sum() 
                     pourcentages = round((grouped / grouped.sum()) * 100, 2)
-                    st.markdown(f"#### 📊 Pourcentage par pays de destination : {selected_fmp_value}")
+                    st.markdown(f"#### 📊 Pourcentage par pays de destination : {selected_fmp_value_01}")
                     st.dataframe(pourcentages.sort_values(ascending=False).head(6))
                 except Exception as e:
                     st.error(f"❌ Erreur lors de l'analyse : {e}")
 
     elif st.session_state.active_analysis == "destination par fmp":
-        st.markdown(f"### 🏁 Analyse par pays de destination pour FMP : {selected_fmp_value}")
+        st.markdown(f"### 🏁 Analyse par pays de destination pour FMP : {selected_fmp_value_01}")
         colnames = df_fmr_filtered.columns.tolist()
-        destination_col = st.selectbox("🌍 Colonne : pays de destination", colnames, key="destination")
-        destination_count_col = st.selectbox("🔢 Colonne : nombre de personnes", colnames, key="destination_count_col")
+        destination_col = st.selectbox("🌍 Colonne : pays de destination", colnames, key="destination_01")
+        destination_count_col = st.selectbox("🔢 Colonne : nombre de personnes", colnames, key="destination_count_col_01")
         if st.button("Lancer l'analyse"):
             if destination_col and destination_count_col:
                 try:
                     df_fmr_filtered[destination_count_col] = pd.to_numeric(df_fmr_filtered[destination_count_col], errors='coerce')
                     grouped = df_fmr_filtered.groupby(destination_col)[destination_count_col].sum()
                     pourcentages = round((grouped / grouped.sum()) * 100, 2)
-                    st.markdown(f"#### 📊 Pourcentage par pays de destination : {selected_fmp_value}")
+                    st.markdown(f"#### 📊 Pourcentage par pays de destination : {selected_fmp_value_01}")
                     st.dataframe(pourcentages.sort_values(ascending=False).head(6))
                 except Exception as e:
                     st.error(f"❌ Erreur lors de l'analyse : {e}")
